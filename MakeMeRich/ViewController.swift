4893835d0113990000c4d046e4e9850b4211c184
Arnaud Dupuy <arnaudupuy62@gmail.com>
2019-06-10 13:21:42 +0200

    Initial Commit

---
+++ MakeMeRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MakeMeRich
+//
+//  Created by Arnaud Dupuy on 10/06/2019.
+//  Copyright © 2019 Arnaud Dupuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
